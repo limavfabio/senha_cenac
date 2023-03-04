a372b82c27206aaf7965fcbb4aa486a868fd7287
Fábio Vinícius Lima <limavfabio@gmail.com>
2023-03-04 14:17:04 -0300

    Add loading state for client
    Add loading message when client connects until
    it receives first message from server

--- components/senhaCounter.vue
+++ components/senhaCounter.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <h3>Senha atual: {{ senhaAtual }}</h3>
+    <h3 v-if="senhaAtual !== 0">Senha atual: {{ senhaAtual }}</h3>
+    <h3 v-else>Por favor, aguarde enquanto a senha é atualizada</h3>
   </div>
 </template>
 
 <script setup lang="ts">
 const supabase = useSupabaseClient();
-const senhaAtual = ref(0);
-
 const channel = supabase.channel("senhaAtual");
+
+const senhaAtual = ref(0);
 
 channel
   .on(
