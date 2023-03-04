c55875e7732380904b8100edbc084a292066c1e7
Fábio Vinícius Lima <limavfabio@gmail.com>
2023-03-04 19:25:18 -0300

    Add vite-pwa
    For PWA support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/supabase", "nuxt-windicss"],
+  modules: ["@nuxtjs/supabase", "nuxt-windicss", "@vite-pwa/nuxt"],
+  pwa: {
+    // PWA options
+    registerType: "autoUpdate",
+    devOptions: {
+      enabled: true,
+    },
+  },
   typescript: {
     strict: true,
   },
